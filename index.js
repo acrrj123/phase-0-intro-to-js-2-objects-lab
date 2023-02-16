b7114785fb284a7be336c3eda3ed3f688e57d1c0
Anna Cole <acrrj123@gmail.com>
2023-02-16 16:02:15 -0600

    commit tree 37ecf65a26228be8ec2fb233c5c90900f54947fc

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: "Ana",
+    streetAddress: "2345 Sage rd",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {...obj, [key]: value};
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+};
+
+function deleteFromEmployeeByKey(obj, key) {
+    let newObj = {...obj };
+    delete newObj.name
+    return newObj;
+};
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    let newObj = obj;
+    delete newObj.name
+    return newObj;
+}
