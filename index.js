d190c605c7921c007914cfd1469e1930d26e0a3b
Anna Cole <acrrj123@gmail.com>
2023-04-19 14:07:09 -0500

    Final changes.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "Ana",
+    streetAddress: "1234 Beautiful st",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {...employee};
+    newObj[key] = value;
+    return newObj;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, name) {
+    const newObj = {...employee};
+    delete newObj.name;
+    return newObj;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name;
+    return employee;
+}
